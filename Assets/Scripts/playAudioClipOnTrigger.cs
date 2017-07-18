f6098ffd68e69667617afe5275331284601c1b6a
Maf'j Alvarez <jay@rootinteractive.com>
2017-07-18 15:31:16 +0100

    [JA] Added loud clucks to chicken prefab to trigger when player collides with it

---
+++ Assets/Scripts/playAudioClipOnTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAudioClipOnTrigger : MonoBehaviour {
+
+	public AudioClip[] ClipToPlay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+
+	void playSound(){
+		GetComponent<AudioSource>().clip = ClipToPlay [Random.Range (0, ClipToPlay.Length)];
+		GetComponent<AudioSource> ().Play ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			playSound();
+			Debug.Log ("played sound");
+		}
+	}
+
+}
