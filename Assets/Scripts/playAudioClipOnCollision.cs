246c82edcef2b360b677d22bd345cb29912e4481
Maf'j Alvarez <jay@rootinteractive.com>
2017-07-18 18:43:13 +0100

    [JA] added creaky wood sounds and script to play an array of sounds on collision with tagged game objects

---
+++ Assets/Scripts/playAudioClipOnCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAudioClipOnCollision : MonoBehaviour {
+
+	public AudioClip[] ClipToPlay;
+	public string[] GameTag;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+
+	void playSound(){
+		GetComponent<AudioSource>().clip = ClipToPlay [Random.Range (0, ClipToPlay.Length)];
+		GetComponent<AudioSource> ().Play();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		foreach(var tag in GameTag){
+			playSound();
+			Debug.Log ("played collision sound");
+
+		}
+	}
+
+}
