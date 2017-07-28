9eb712b9fb03c1eda810d11d2cc9e60d622a48fd
Tintincita <merlincita@gmail.com>
2017-07-28 17:49:04 +0100

    [MK] Added ThrowEgg scene

---
+++ Assets/Scripts/EggScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EggScript : MonoBehaviour {
+
+	public GameObject parentBone;
+	public Rigidbody rigid;
+
+
+	// Use this for initialization
+	void Start () {
+		transform.parent = parentBone.transform;
+		rigid.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ReleaseMe () {
+		transform.parent = null;
+		rigid.useGravity = true;
+		//transform.rotation = parentBone.transform.rotation;
+		//rigid.AddForce (transform.forward * 20000);
+
+	}
+}
---
+++ Assets/Throw.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour {
+
+	public GameObject theObject;
+	private EggScript objectScript;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ThrowObject () {
+		Debug.Log("Throwiiing");
+		theObject.GetComponent <EggScript> ().ReleaseMe();
+	}
+}
