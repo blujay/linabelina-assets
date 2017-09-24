dd121807080fdc864f4f8657515b73087577e576
Maf'j Alvarez <jay@rootinteractive.com>
2017-09-24 20:29:29 +0100

    [JA] added credits screen and hooked to intro screen

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,16 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
 
+	public Button startButton;
+	public Button closeControlsButton;
+	public Canvas startMenu;
+	public Canvas controlsMenu;
+	public Canvas credits;
+	public Button closeCreditsButton;
+	public Button controlsText;
+	public string sceneToLoad;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		controlsText = controlsText.GetComponent<Button> ();
+		controlsMenu = controlsMenu.GetComponent<Canvas> ();
+		startMenu = startMenu.GetComponent<Canvas> ();
+		credits = credits.GetComponent<Canvas> ();
+		startButton = startButton.GetComponent<Button> ();
+		closeControlsButton = closeControlsButton.GetComponent<Button> ();
+		controlsMenu.enabled = false;
+		credits.enabled = false;
+		startMenu.enabled = true;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public void StartPress(){
+		controlsMenu.enabled = true;
+		startMenu.enabled = false;
 	}
+
+	public void LoadScene(){
+		SceneManager.LoadScene (sceneToLoad);
+	}
+
+	public void LoadCredits(){
+		credits.enabled = true;
+		startMenu.enabled = false;
+	}
+
+	public void CloseControlMenu(){
+		controlsMenu.enabled = false;
+		startMenu.enabled = true;
+	}
+
+	public void CloseCredits(){
+		credits.enabled = false;
+		startMenu.enabled = true;
+	}
+
+
 }
