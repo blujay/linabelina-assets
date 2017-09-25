814cf2fcf4f19ccd1642b47a0f834f4d2d9b8799
Maf'j Alvarez <jay@rootinteractive.com>
2017-09-25 15:40:02 +0100

    [JA] generalising scripts to allow any object to be dropped.

---
+++ Assets/Scripts/ObjectScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectScript : MonoBehaviour {
+
+	public GameObject throwPrefab;
+	private GameObject parentBone;
+	private GameObject handheldObject;
+	private Color initialColor;
+
+
+	// Use this for initialization
+	void Start () {
+		initialColor = gameObject.GetComponent<Renderer>().material.color;
+	}
+
+	public void CloneMe(){
+		parentBone = GameObject.Find("HandHolder");
+		handheldObject = Instantiate (throwPrefab, parentBone.transform) as GameObject;
+		handheldObject.name = "Handheld object";
+		Debug.Log ("cloned");
+		handheldObject.transform.localPosition = Vector3.zero;
+		handheldObject.GetComponent<MeshCollider> ().enabled = false;
+		handheldObject.GetComponent<Rigidbody> ().useGravity = false;
+
+	}
+
+	public void ReleaseMe () {
+		handheldObject.GetComponent<Rigidbody> ().useGravity = true;
+		this.GetComponent<MeshCollider> ().enabled = true;
+		this.transform.parent = null;
+
+	}
+
+	public void HighlightOn(){
+			gameObject.GetComponent<Renderer>().material.color = Color.white;
+	}
+
+	
+	public void HighlightOff() {
+			gameObject.GetComponent<Renderer>().material.color = initialColor;
+	}
+}
--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -4,40 +4,29 @@ using UnityEngine;
 
 public class Throw : MonoBehaviour {
 
-	public GameObject eggPrefab;
-	public GameObject staticEggPrefab;
-	private GameObject newObject;
-	private EggScript objectScript;
+	public GameObject throwPrefab;
+	private ObjectScript objectScript;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	void CloneObject(){
 		//Debug.Log("Cloning");
-		eggPrefab.GetComponent <EggScript> ().CloneMe();
+		throwPrefab.GetComponent <ObjectScript> ().CloneMe();
 	}
 
 	void ThrowObject () {
 		//Debug.Log("Throwiiing");
-		newObject = GameObject.Find ("Current Clone");
-		newObject.GetComponent <EggScript> ().ReleaseMe();
-		StartCoroutine(stopRolling());
+		throwPrefab.GetComponent <ObjectScript> ().ReleaseMe();
+		//StartCoroutine(stopRolling());
 	}
 
-		IEnumerator stopRolling(){
-			print(Time.time);
-			yield return new WaitForSeconds(5);
-			Vector3 eggPosition = newObject.transform.position;
-			Debug.Log (eggPosition);
-			//GameObject NewEgg = Instantiate (staticEggPrefab, eggPosition) as GameObject;
-			
-		}
+//		IEnumerator stopRolling(){
+//			print(Time.time);
+//			yield return new WaitForSeconds(5);
+//			Vector3 objectPosition = transform.position;
+//			Debug.Log (objectPosition);
+//			//GameObject NewEgg = Instantiate (staticEggPrefab, eggPosition) as GameObject;
+//			
+//		}
 	}
 
 
